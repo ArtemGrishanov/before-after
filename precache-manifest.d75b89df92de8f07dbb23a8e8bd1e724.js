6f39752b36468ce3b91415143e77517b3b3f71bc
artyomgrishanov-qs <agrishanov@ambrahealth.com>
2020-06-15 19:37:02 +0300

    Updates

---
+++ precache-manifest.d75b89df92de8f07dbb23a8e8bd1e724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "005fdcecca479d3fdcb1",
+    "url": "/static/js/main.005fdcec.chunk.js"
+  },
+  {
+    "revision": "a961ab5bf326ad4496f1",
+    "url": "/static/js/1.a961ab5b.chunk.js"
+  },
+  {
+    "revision": "005fdcecca479d3fdcb1",
+    "url": "/static/css/main.93b32b12.chunk.css"
+  },
+  {
+    "revision": "a77bebd7281e2add037a82aeab872ee2",
+    "url": "/index.html"
+  }
+];
